766caa73d7850551cb6418042111b99c063f4859
sebasdex <sebasdc9@hotmail.com>
2025-06-04 14:55:47 -0600

    refact: Refactor routing in main.tsx to include Favorites page

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router";
 import "./index.css";
 import App from "./App.tsx";
+import Favorites from "./pages/favorites/components/Favorites.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<App />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   </StrictMode>
 );
