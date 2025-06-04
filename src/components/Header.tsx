a81521b30f393c8b27095a54c5877cce71b31fa5
sebasdex <sebasdc9@hotmail.com>
2025-06-04 14:09:10 -0600

    initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,32 @@
+import { Heart } from "lucide-react";
+import { Link } from "react-router";
+
+function Header() {
+  return (
+    <header className="bg-gray-800 text-white shadow-md px-6 py-4">
+      <div className="max-w-6xl mx-auto flex flex-col md:flex-row items-center md:justify-between gap-4">
+        {/* Title */}
+        <Link to="/">
+          <h1 className="text-2xl sm:text-3xl font-bold tracking-wide text-center sm:text-left">
+            Rick & Morty
+          </h1>
+        </Link>
+
+        {/* Favorites button */}
+        <Link
+          to={"/favorites"}
+          className="relative flex items-center gap-2 bg-gray-700 hover:bg-gray-600 px-4 py-2 rounded-full transition-colors hover:cursor-pointer"
+          aria-label="Favoritos"
+        >
+          <Heart className="w-5 h-5" />
+          <span className="text-base font-medium">Favoritos</span>
+          <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs font-bold w-5 h-5 flex items-center justify-center rounded-full shadow">
+            0
+          </span>
+        </Link>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
