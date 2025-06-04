7c757ba7503559d349d321fc1f8f67ce197d2035
sebasdex <sebasdc9@hotmail.com>
2025-06-04 17:24:48 -0600

    feat: Add Outlet component for nested routing in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import Header from "./components/Header";
-
+import { Outlet } from "react-router";
 function App() {
   return (
     <div>
       <Header />
+      <Outlet />
     </div>
   );
 }
