96f34f4314ad70e9b6efc28e8e8e0f785038b8ae
sebasdex <sebasdc9@hotmail.com>
2025-06-04 17:25:15 -0600

    refact: Remove notification badge from Favorites link in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,9 +20,6 @@ function Header() {
         >
           <Heart className="w-5 h-5" />
           <span className="text-base font-medium">Favoritos</span>
-          <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs font-bold w-5 h-5 flex items-center justify-center rounded-full shadow">
-            0
-          </span>
         </Link>
       </div>
     </header>
