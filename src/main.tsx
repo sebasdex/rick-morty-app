e67d4b28f059fdf029dc3b1dccb8a7c67970a4fc
sebasdex <sebasdc9@hotmail.com>
2025-06-04 18:28:32 -0600

    feat: Add route for character details page in main application

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "./index.css";
 import App from "./App.tsx";
 import Favorites from "./pages/favorites/components/Favorites.tsx";
 import Home from "./pages/home/Home.tsx";
+import CharacterDetails from "./pages/characters/CharacterDetails.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -13,6 +14,7 @@ createRoot(document.getElementById("root")!).render(
         <Route path="/" element={<App />}>
           <Route index element={<Home />} />
           <Route path="/favorites" element={<Favorites />} />
+          <Route path="/character/:id" element={<CharacterDetails />} />
         </Route>
       </Routes>
     </BrowserRouter>
